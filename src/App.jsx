a889947b27f8311f89a39bb26ed94b14837d7d41
Leah Stone <leah.stone@force24.co.uk>
2025-11-05 17:21:36 +0000

    expected fields and actual JSON output

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,21 @@ import SummaryPanel from "./components/SummaryPanel.jsx";
 import TransactionContext from "./components/TransactionContext.jsx";
 import Header from "./components/ui/header.jsx";
 import UploadSection from "./components/UploadSection.jsx";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [expectedFields, setExpectedFields] = useState([]);
+  const [actualJson, setActualJson] = useState(null);
+
+  // test for console
+   useEffect(() => {
+    console.log("Expected Fields changed:", expectedFields);
+  }, [expectedFields]);
+
+  useEffect(() => {
+    console.log("Actual JSON changed:", actualJson);
+  }, [actualJson]);
+
   return (
     <div className="App">
       <Header className="header-component" />
@@ -14,10 +27,14 @@ function App() {
         <UploadSection
           title="Expected Fields"
           description="Upload or paste the list of fields that should exist in the workflow output"
+          type="fields"
+          onChange={setExpectedFields} 
         />
-        <UploadSection
+         <UploadSection
           title="Actual Output (JSON)"
           description="Paste the JSON response from Cyclr transaction logs"
+          type="json"
+          onChange={setActualJson}
         />
       </div>
       <TransactionContext className="transaction-component" />
